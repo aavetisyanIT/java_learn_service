5653255729002bdca6a51822f96577387653ec59
Artur <aavetisyanit@gmail.com>
2022-05-23 18:23:58 -0500

    array basics

--- business/src/NewsAgency.java
+++ business/src/NewsAgency.java
@@ -1,5 +1,7 @@
-import companyA.Company;
-
 class NewsAgency {
-    Company company;
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        System.out.println(nums[0]);
+        System.out.println("Array length is " + nums.length);
+    }
 }
