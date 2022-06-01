0631a804f88cb90cd3486ccacfceb7590d72a37a
Artur <aavetisyanit@gmail.com>
2022-06-01 17:12:28 -0500

    add quite functionality for guessing game

--- People/src/GuessIt.java
+++ People/src/GuessIt.java
@@ -4,15 +4,19 @@ import java.util.Random;
 public class GuessIt {
     public static void main(String[] args) {
         int randomNum = new Random().nextInt(10) + 1;
-        while(true) {
-            String userInput = System.console().readLine("Please, guess a number: ");
-            int guessedNum = Integer.parseInt(userInput);
-            if(guessedNum == randomNum) {
-                System.out.printf("The random number was %d, You got it!%n", randomNum);
-                return;
-            } else {
-                System.out.println("Try again!");
-            };
+        String userInput = null;
+        while(!"q".equals(userInput)) {
+            userInput = System.console().readLine("Please, guess a number: ");
+            if (userInput.matches("-?\\d{1,2}")) {
+                int guessedNum = Integer.parseInt(userInput);
+                if(guessedNum == randomNum) {
+                    System.out.printf("The random number was %d, You got it!%n", randomNum);
+                    return;
+                } else {
+                    System.out.println("Try again!");
+                }
+                ;
+            }
         }
 
     }
