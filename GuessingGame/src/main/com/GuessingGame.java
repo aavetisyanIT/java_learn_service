5238d635512b198fd6bd30889f175da3929a205c
Artur <aavetisyanit@gmail.com>
2022-06-09 16:25:35 -0500

    create generateRandomNum method and test

--- GuessingGame/src/main/com/GuessingGame.java
+++ GuessingGame/src/main/com/GuessingGame.java
@@ -1,7 +1,14 @@
 package com;
 
+import java.util.Random;
+
 public class GuessingGame {
-    public String quessEnteredNum(int guessedNum) {
+    public String guessEnteredNum(int guessedNum) {
         return "You got it";
+    }
+
+    public int generateRandomNum() {
+        Random randomNum = new Random();
+        return randomNum.nextInt(10) + 1;
     }
 }
--- GuessingGame/src/test/com/GuessingGameTest.java
+++ GuessingGame/src/test/com/GuessingGameTest.java
@@ -1,13 +1,36 @@
 package com;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
+
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GuessingGame();
+    }
+
     @Test
     public void testWinSituation () {
-        GuessingGame game = new GuessingGame();
-        String result = game.quessEnteredNum(3);
+        String result = game.guessEnteredNum(3);
         assertEquals("You got it", result);
+    }
+
+    @Test
+    public void testRandomNumberGeneration () {
+        int[] rndNumMap = new int[10];
+        for(int i = 1; i <= 100; i++) {
+            int randomNum = game.generateRandomNum();
+            rndNumMap[randomNum-1] = 1;
+        }
+        int sum = IntStream.of(rndNumMap).sum();
+        assertEquals(10, sum);
     }
 }
